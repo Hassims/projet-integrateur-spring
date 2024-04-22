fe5dfab5015e08af213b5ce561fd4075689c6b1a
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-04-22 20:34:50 +0200

    Ajout de 3 méthodes de mapping à TourneeMapper.

--- server/src/main/java/fr/uga/l3miage/integrator/mappers/TourneeMapper.java
+++ server/src/main/java/fr/uga/l3miage/integrator/mappers/TourneeMapper.java
@@ -1,15 +1,28 @@
 package fr.uga.l3miage.integrator.mappers;
 
-import fr.uga.l3miage.integrator.models.JourneeEntity;
-import fr.uga.l3miage.integrator.models.TourneeEntity;
+import fr.uga.l3miage.integrator.models.*;
 import fr.uga.l3miage.integrator.response.VisualiserUneTourneeDTO;
 import org.mapstruct.Mapper;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Mapper
 public interface TourneeMapper {
+
     VisualiserUneTourneeDTO toResponse(TourneeEntity playlistEntity);
 
-    default String map(JourneeEntity journee) {
+    default String mapJournee(JourneeEntity journee) {
         return journee.getDate().toString();
+    }
+
+    default String mapCamion(CamionEntity camion) { return camion.getImmatriculation(); }
+
+    default Set<String> mapSetEmploye(Set<EmployeEntity> employes) {
+        return employes.stream().map(EmployeEntity::getTrigramme).collect(Collectors.toSet());
+    }
+
+    default Set<String> mapSetCommande(Set<CommandeEntity> commandes) {
+        return commandes.stream().map(CommandeEntity::getReference).collect(Collectors.toSet());
     }
 }
