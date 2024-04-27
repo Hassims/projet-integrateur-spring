c1ebee63bd60c42c24724ce2d1825ed929897f8b
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-04-27 10:50:54 +0200

    Utilisation d'un Optional.

--- server/src/main/java/fr/uga/l3miage/integrator/services/VisualiserTourneeService.java
+++ server/src/main/java/fr/uga/l3miage/integrator/services/VisualiserTourneeService.java
@@ -1,9 +1,12 @@
 package fr.uga.l3miage.integrator.services;
 
 import fr.uga.l3miage.integrator.components.TourneeComponent;
+import fr.uga.l3miage.integrator.exceptions.rest.NotFoundEntityRestException;
 import fr.uga.l3miage.integrator.models.TourneeEntity;
 import fr.uga.l3miage.integrator.repositories.TourneeRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
 
 @org.springframework.stereotype.Service
 @RequiredArgsConstructor
@@ -22,7 +25,14 @@ public class VisualiserTourneeService {
     }
     */
 
-    public TourneeEntity findTournee(String reference) {
-        return tourneeComponent.findByReference(reference);
+    public TourneeEntity findTournee(String reference) throws NotFoundEntityRestException {
+        Optional<TourneeEntity> optTournee = tourneeComponent.findByReference(reference);
+
+        if (optTournee.isPresent()) {
+            return optTournee.get();
+        }
+        else {
+            throw new NotFoundEntityRestException("Tournée non trouvée");
+        }
     }
 }
