b5ce695d528fecd2006c3e8d24d7b2be74a01bae
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-04-27 10:51:34 +0200

    Changement de l'URL de l'API "/api/v1" -> "/api/"

--- rest-api/src/main/java/fr/uga/l3miage/integrator/endpoints/VisualiserTourneeEndpoints.java
+++ rest-api/src/main/java/fr/uga/l3miage/integrator/endpoints/VisualiserTourneeEndpoints.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Tag(name = "VisualiserUneTourneeEndpoints")
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/")
 public interface VisualiserTourneeEndpoints {
     /*
     @Operation(description = "Récupère tous les entrepôts.")
