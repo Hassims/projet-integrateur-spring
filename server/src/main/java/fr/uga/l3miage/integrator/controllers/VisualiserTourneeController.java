5d509c202f97827939eccdc1e215b634b54e13e6
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-04-29 22:05:46 +0200

    Spécification et refactorisation

--- server/src/main/java/fr/uga/l3miage/integrator/controllers/VisualiserTourneeController.java
+++ server/src/main/java/fr/uga/l3miage/integrator/controllers/VisualiserTourneeController.java
@@ -1,21 +1,16 @@
 package fr.uga.l3miage.integrator.controllers;
 
 import fr.uga.l3miage.integrator.endpoints.VisualiserTourneeEndpoints;
-import fr.uga.l3miage.integrator.exceptions.rest.NotFoundEntityRestException;
 import fr.uga.l3miage.integrator.mappers.EntrepotMapper;
 import fr.uga.l3miage.integrator.mappers.TourneeMapper;
-import fr.uga.l3miage.integrator.models.EntrepotEntity;
-import fr.uga.l3miage.integrator.models.TourneeEntity;
-import fr.uga.l3miage.integrator.response.EntrepotsDTO;
+import fr.uga.l3miage.integrator.response.EntrepotDTO;
+import fr.uga.l3miage.integrator.response.JourneeDTO;
 import fr.uga.l3miage.integrator.response.VisualiserUneTourneeDTO;
 import fr.uga.l3miage.integrator.services.VisualiserTourneeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashSet;
 import java.util.Set;
 
 @RestController
@@ -23,33 +18,21 @@ import java.util.Set;
 public class VisualiserTourneeController implements VisualiserTourneeEndpoints {
 
     private final VisualiserTourneeService service;
-    private final TourneeMapper mapper;
+    private final TourneeMapper tourneeMapper;
     private final EntrepotMapper entrepotMapper;
 
-    /**
-     * @param reference
-     * @return
-     */
     @Override
-    public ResponseEntity<VisualiserUneTourneeDTO> getVisuTournee(@PathVariable String reference) {
-        try {
-            TourneeEntity tourneeEntity = service.findTournee(reference);
-            VisualiserUneTourneeDTO tourneeDTO = mapper.toResponse(tourneeEntity);
-            return ResponseEntity.ok(tourneeDTO);
-        } catch (NotFoundEntityRestException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+    public VisualiserUneTourneeDTO getVisuTournee(@PathVariable String reference) {
+        return tourneeMapper.toResponse(service.findTournee(reference));
     }
 
     @Override
-    public ResponseEntity<Set<EntrepotsDTO>> getAllEntrepot() {
-        Set<EntrepotEntity> set = service.getAllEntrepots();
-        Set<EntrepotsDTO> setDTO = new HashSet<>();
-        for (EntrepotEntity entrepot : set) {
-            setDTO.add(entrepotMapper.toDTO(entrepot));
-        }
-        return ResponseEntity.ok(setDTO);
+    public Set<EntrepotDTO> getAllEntrepot() {
+        return entrepotMapper.toDTOSet(service.getAllEntrepots());
     }
 
-
+    @Override
+    public JourneeDTO getJourneeByEntrepotAndDate(String nomEntrepot, String dateJournee) {
+        return null;
+    }
 }
--- server/src/test/java/fr/uga/l3miage/integrator/controllers/VisualiserTourneeControllerTest.java
+++ server/src/test/java/fr/uga/l3miage/integrator/controllers/VisualiserTourneeControllerTest.java
@@ -1,23 +1,22 @@
 package fr.uga.l3miage.integrator.controllers;
 
 import com.google.gson.Gson;
-import fr.uga.l3miage.integrator.enums.EtatsDeTournee;
-import fr.uga.l3miage.integrator.repositories.CommandeRepository;
-import fr.uga.l3miage.integrator.repositories.LivraisonRepository;
-import fr.uga.l3miage.integrator.repositories.TourneeRepository;
-import fr.uga.l3miage.integrator.requests.TourneePatchRequest;
-import fr.uga.l3miage.integrator.services.ChargerTourneeService;
+import fr.uga.l3miage.integrator.mappers.EntrepotMapper;
+import fr.uga.l3miage.integrator.models.EntrepotEntity;
+import fr.uga.l3miage.integrator.repositories.EntrepotRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -26,19 +25,44 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class VisualiserTourneeControllerTest {
     @Autowired
     private MockMvc mockMvc;
-    @SpyBean
-    private ChargerTourneeService service;
     @Autowired
-    private TourneeRepository tourneeRepository;
+    private EntrepotRepository entrepotRepository;
     @Autowired
-    private LivraisonRepository livraisonRepository;
-    @Autowired
-    private CommandeRepository commandeRepository;
+    private EntrepotMapper entrepotMapper;
+
+    @AfterEach
+    void clean() {
+        entrepotRepository.deleteAll();
+    }
 
     @Test
-    void getAllEntrepot() throws Exception {
-        final MockHttpServletRequestBuilder request = get("/entrepot");
+    void getAllEntrepotEmpty() throws Exception {
+        final MockHttpServletRequestBuilder request = get("/api/entrepot");
 
-        mockMvc.perform(request).andExpect(status().isFound());
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    void getAllEntrepotNotEmpty() throws Exception {
+        EntrepotEntity e1 = EntrepotEntity.builder()
+                .nom("Grenis").lettre("G").adresse("").codePostal("00000").ville("").build();
+
+        EntrepotEntity e2 = EntrepotEntity.builder()
+                .nom("Bronis").lettre("B").adresse("").codePostal("00000").ville("").build();
+
+        EntrepotEntity e3 = EntrepotEntity.builder()
+                .nom("Albis").lettre("A").adresse("").codePostal("00000").ville("").build();
+
+        List<EntrepotEntity> entityList = List.of(e1, e2, e3);
+
+        entrepotRepository.saveAll(entityList);
+
+        String expectedJsonResult = new Gson().toJson(entrepotMapper.toDTOList(entityList));
+
+        mockMvc.perform(get("/api/entrepot"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJsonResult));
     }
 }
