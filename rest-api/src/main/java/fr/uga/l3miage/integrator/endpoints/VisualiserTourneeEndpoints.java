37edb3f7930899cc8be588c1dfce639961426fc0
l3miage-haddaray <rayane.haddad@etu.univ-grenoble-alpes.fr>
2024-04-30 11:20:27 +0200

    Test visuTournee(found/notFound)

--- rest-api/src/main/java/fr/uga/l3miage/integrator/endpoints/VisualiserTourneeEndpoints.java
+++ rest-api/src/main/java/fr/uga/l3miage/integrator/endpoints/VisualiserTourneeEndpoints.java
@@ -19,18 +19,21 @@ public interface VisualiserTourneeEndpoints {
 
     @Operation(description = "Récupère la visualisation d'une tournée..")
     @ApiResponse(responseCode= "200", description = "Un objet qui décrit la tournée et ses objets associés.")
+    @ApiResponse(responseCode= "404", description = "Aucune visualisation de tournee trouvée.")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/tournee/{reference}/visu")
     VisualiserUneTourneeDTO getVisuTournee(@PathVariable(name = "reference")String reference);
 
     @Operation(description = "Récupère tous les entrepots")
     @ApiResponse(responseCode= "200", description = "Une liste d'entrepots")
+    @ApiResponse(responseCode= "404", description = "Aucun entrepots trouvée.")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/entrepot")
     Set<EntrepotDTO>getAllEntrepot();
 
     @Operation(description = "Récupère une journée associée à un entrepot et une date.")
     @ApiResponse(responseCode= "200", description = "Une journée")
+    @ApiResponse(responseCode= "404", description = "Journée non trouvée.")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/entrepot/{nom}/journee/{date}")
     JourneeDTO getJourneeByEntrepotAndDate(@PathVariable(name = "nom")String nomEntrepot, @PathVariable(name = "date")String dateJournee);
--- server/src/test/java/fr/uga/l3miage/integrator/controllers/VisualiserTourneeControllerTest.java
+++ server/src/test/java/fr/uga/l3miage/integrator/controllers/VisualiserTourneeControllerTest.java
@@ -1,9 +1,17 @@
 package fr.uga.l3miage.integrator.controllers;
 
 import com.google.gson.Gson;
+import fr.uga.l3miage.integrator.enums.EtatsDeJournee;
+import fr.uga.l3miage.integrator.enums.EtatsDeTournee;
 import fr.uga.l3miage.integrator.mappers.EntrepotMapper;
+import fr.uga.l3miage.integrator.models.CamionEntity;
 import fr.uga.l3miage.integrator.models.EntrepotEntity;
+import fr.uga.l3miage.integrator.models.JourneeEntity;
+import fr.uga.l3miage.integrator.models.TourneeEntity;
+import fr.uga.l3miage.integrator.repositories.CamionRepository;
 import fr.uga.l3miage.integrator.repositories.EntrepotRepository;
+import fr.uga.l3miage.integrator.repositories.JourneeRepository;
+import fr.uga.l3miage.integrator.repositories.TourneeRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +21,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -27,6 +37,14 @@ public class VisualiserTourneeControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private EntrepotRepository entrepotRepository;
+    @Autowired
+    private JourneeRepository journeeRepository ;
+    @Autowired
+    private TourneeRepository tourneeRepository ;
+    @Autowired
+    private CamionRepository camionRepository ;
+
+
     @Autowired
     private EntrepotMapper entrepotMapper;
 
@@ -64,5 +82,63 @@ public class VisualiserTourneeControllerTest {
         mockMvc.perform(get("/api/entrepot"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(expectedJsonResult));
+    }
+
+    @Test
+    void getVisuTournee() throws Exception {
+        CamionEntity visuCamion = CamionEntity.builder()
+                .immatriculation("XY-456-ZT")
+                .build();
+        camionRepository.save(visuCamion) ;
+
+        EntrepotEntity visuEntrepot = EntrepotEntity.builder()
+                .nom("Grenis")
+                .lettre("G")
+                .adresse("")
+                .codePostal("38000")
+                .ville("Grenoble")
+                .camions(Set.of(visuCamion))
+                .build();
+        entrepotRepository.save(visuEntrepot) ;
+
+
+        JourneeEntity visuJournee = JourneeEntity.builder()
+                .etat(EtatsDeJournee.planifiee)
+                .entrepot(visuEntrepot)
+                .date(LocalDate.of(2024,1,28))
+                .build();
+
+        journeeRepository.save(visuJournee) ;
+
+        TourneeEntity visuTournee = TourneeEntity.builder()
+                .etat(EtatsDeTournee.planifiee)
+                .lettre("A")
+                .journee(visuJournee)
+                .camion(visuCamion)
+                .build();
+
+        tourneeRepository.save(visuTournee) ;
+        journeeRepository.save(visuJournee) ;
+
+        visuJournee.setTournees(Set.of(visuTournee));
+
+        entrepotRepository.save(visuEntrepot) ;
+
+        String reference = visuTournee.getReference();
+
+        // Act & Assert
+        mockMvc.perform(get("/api/tournee/"+reference+"/visu"))
+                .andExpect(status().isOk());
+
+    }
+
+
+    @Test
+    void getVisuTourneeNotFound() throws Exception {
+
+        // Act & Assert
+        mockMvc.perform(get("/api/tournee/t028G-A/visu"))
+                .andExpect(status().isNotFound()) ;
+
     }
 }
