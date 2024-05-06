d6fbae492487e0ae895a25dcf76844f2e1ce1762
l3miage-severinm <matteo.severini@etu.univ-grenoble-alpes.fr>
2024-05-06 07:40:07 +0200

    Implémentation de la visualisation de livraisons.

--- server/src/main/java/fr/uga/l3miage/integrator/mappers/VisualiserLivraisonMapper.java
+++ server/src/main/java/fr/uga/l3miage/integrator/mappers/VisualiserLivraisonMapper.java
@@ -3,15 +3,13 @@ package fr.uga.l3miage.integrator.mappers;
 import fr.uga.l3miage.integrator.models.CommandeEntity;
 import fr.uga.l3miage.integrator.models.LivraisonEntity;
 import fr.uga.l3miage.integrator.response.VisualiserLivraisonDTO;
-import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import java.util.stream.Collectors;
 
-@Mapper
-public interface VisualiserLivraisonMapper {
+public class VisualiserLivraisonMapper {
 
-    default VisualiserLivraisonDTO toDTO(LivraisonEntity entity) {
+    VisualiserLivraisonDTO toDTO(LivraisonEntity entity) {
         return VisualiserLivraisonDTO.builder()
                 .reference(entity.getReference())
                 .numero(entity.getNumero())
