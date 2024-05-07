aed7c2d07368646b23494000966537c9a9a9efef
l3miage-haddaray <rayane.haddad@etu.univ-grenoble-alpes.fr>
2024-05-07 10:53:57 +0200

    patch journeedate

--- server/src/main/java/fr/uga/l3miage/integrator/services/JourneeService.java
+++ server/src/main/java/fr/uga/l3miage/integrator/services/JourneeService.java
@@ -1,7 +1,11 @@
 package fr.uga.l3miage.integrator.services;
 
+import fr.uga.l3miage.integrator.components.JourneeComponent;
+import fr.uga.l3miage.integrator.enums.EtatsDeJournee;
+import fr.uga.l3miage.integrator.enums.EtatsDeTournee;
 import fr.uga.l3miage.integrator.exceptions.rest.NotFoundEntityRestException;
 import fr.uga.l3miage.integrator.models.JourneeEntity;
+import fr.uga.l3miage.integrator.models.TourneeEntity;
 import fr.uga.l3miage.integrator.repositories.JourneeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,11 +17,25 @@ import java.time.LocalDate;
 public class JourneeService {
 
     private final JourneeRepository journeeRepository;
+    private final JourneeComponent journeeComponent;
 
     public JourneeEntity findJourneeByEntrepotAndDate(String entrepotNom, LocalDate localDate) {
         JourneeEntity journee = journeeRepository.findByEntrepot_NomAndDateEquals(entrepotNom, localDate);
         if (journee == null)
             throw new NotFoundEntityRestException("Journée non trouvée");
         return journee;
+    }
+
+    public JourneeEntity updateJourneeDate(String reference, LocalDate date ) {
+
+        JourneeEntity journee;
+        try {
+            journee = journeeComponent.findByReference(reference);
+        } catch (Exception e) {
+            throw new NotFoundEntityRestException("Journée non trouvée");
+        }
+
+        journee.setDate(date);
+        return journeeRepository.save(journee);
     }
 }
